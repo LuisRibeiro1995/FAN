b1efe81b238d603c91393c554b60ac2eebf0fdf2
Siham <46928466+siham87@users.noreply.github.com>
2019-01-31 15:42:05 +0100

    Add files via upload

---
+++ FAN.ino
@@ -0,0 +1,39 @@
+#include <boarddefs.h>
+#include <IRremote.h>
+#include <IRremoteInt.h>
+#include <ir_Lego_PF_BitStreamEncoder.h>
+
+
+IRrecv irReceiver(2);
+decode_results results;
+
+#include <L293D.h>
+L293D motor(6,5,4);
+
+
+void setup() {
+
+  irReceiver.enableIRIn(); 
+  Serial.begin(9600);
+}
+
+
+void loop() {
+
+  if (irReceiver.decode(&results)) {
+    // le code infrarouge est dans la variable results.value
+    irReceiver.resume();
+
+
+  if (results.value == 16753245) {
+  motor.set(200);
+  }  
+    
+  Serial.println(results.value);
+
+   if (results.value == 16738455) {
+   motor.set(0);  
+      }
+
+  }
+}
